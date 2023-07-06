441dcb9f78126cd862aa87716937369bb75ef26d
Pierre Boudvillain <pierre.boudvillain@etu.imt-nord-europe.fr>
2023-07-06 15:18:37 +0200

    pwn

---
+++ pwn/l-innondation/main.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+short tuyaux[] = {1, 0, 0, 1, 1, 0, 0};
+char command[3];
+
+int main(int argc, char **argv) {
+
+    char noteEquipe[20];
+    int tuyauActuel = 4;
+
+    while(1) {
+        printf("Status des tuyaux: |");
+        for (int i=0; i<7; i++){
+            if (i == tuyauActuel) printf("*");
+            printf(tuyaux[i] == 0 ? "Ouvert|" : "Fermé|");
+        } 
+        printf("\nLevier à actionner:\n1) Fermeture du tuyau\n2) Ouverture du tuyau\n3) Sélecteur de tuyau\n4) Laisser une note à l'équipe\n0) Prévenir le chef que vous avez fermé toutes les vannes\n");
+        fgets(command, sizeof(command), stdin);
+        if (command[0] == '1') {
+            tuyaux[tuyauActuel] = 1;
+        } else if (command[0] == '2') {
+            tuyaux[tuyauActuel] = 0;
+        } else if (command[0] == '3') {
+            printf("*le levier semble coincé et refuse de bouger...*\n");
+        } else if (command[0] == '4') {
+            printf("*vous prenez un post-it et un crayon et commencez à écrire:*\n");
+            gets(noteEquipe);
+        } else if (command[0] == '0') {
+            short valid = 1;
+            for (int i=0; i<7; i++){
+                if (tuyaux[i] == 0) {
+                    valid = 0;
+                    break;
+                }
+            }
+            if (valid == 1) {
+                FILE *f;
+                f = fopen("flag.txt", "r");
+                fseek(f, 0L, SEEK_END);
+                int size = ftell(f) + 1;
+                fseek(f, 0L, SEEK_SET);
+                char* content = malloc(size);
+                fgets(content, size, f);
+
+                fclose(f);
+                printf("Votre chef vous félicite et vous remet ceci: %s\n", content);
+                break;
+            } else {
+                printf("Votre chef fronce les sourcils et vous jette un regard noir, il n'a pas l'air satisfait de votre travail.\n");
+            }
+        }
+    }
+
+
+
+    return 0;
+}
